6d27ee090be78c5beebc5576a84a2a819e48b3fd
Angelyn <127252122+QuinainAngel@users.noreply.github.com>
2024-05-12 18:11:30 -0500

    Added a function to prompt user input
    Prompts user input and checks if it matches one the options regardless of capitalization

--- javascript.js
+++ javascript.js
@@ -1,11 +1,32 @@
 let computerSelection;
+let humanSelection;
 let RPS_Options = ["rock","paper","scissors"];
 
+//Randomizes Computer Choice of: rock, paper, or scissors
 function getComputerChoice () {
     let randomCompChoice = Math.floor(Math.random() * RPS_Options.length);
     let compChoice = RPS_Options[randomCompChoice];
     return compChoice;
 }
 
+//Prompts user to input rock, paper, or scissors
+function getHumanChoice() {
+    let humanChoice;
+    while (true) {
+        humanChoice = prompt("Enter your choice: rock, paper, or scissors").toLowerCase();
+        //checks if user's input is one of the options from the RPS_Options array
+        //Will prompt user input until it matches one of the choices
+        if (RPS_Options.includes(humanChoice)) {
+            return humanChoice;
+            break;
+        } else {
+            console.log ("Please choose rock, paper, or scissors");
+        }
+    }
+}
+
 computerSelection = getComputerChoice();
 console.log(computerSelection);
+
+humanSelection = getHumanChoice();
+console.log(humanSelection);
