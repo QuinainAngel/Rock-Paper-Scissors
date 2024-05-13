4e561be12f2e06966915d4f5179358f94fc50b21
Angelyn <127252122+QuinainAngel@users.noreply.github.com>
2024-05-13 17:42:48 -0500

    Created playRound function to test win conditions
    Coded playRound function that takes user input and compares it against computer choice to see who wins

--- javascript.js
+++ javascript.js
@@ -1,12 +1,14 @@
 let computerSelection;
 let humanSelection;
+let humanScore = 0;
+let computerScore = 0;
 let RPS_Options = ["rock","paper","scissors"];
 
 //Randomizes Computer Choice of: rock, paper, or scissors
 function getComputerChoice () {
     let randomCompChoice = Math.floor(Math.random() * RPS_Options.length);
-    let compChoice = RPS_Options[randomCompChoice];
-    return compChoice;
+    let computerChoice = RPS_Options[randomCompChoice];
+    return computerChoice;
 }
 
 //Prompts user to input rock, paper, or scissors
@@ -20,7 +22,40 @@ function getHumanChoice() {
             return humanChoice;
             break;
         } else {
-            console.log ("Please choose rock, paper, or scissors");
+            console.log ("Please choose: rock, paper, or scissors");
+        }
+    }
+}
+
+function playRound(humanChoice, computerChoice) {
+    //Tests win conditions when user inputs rock
+    if (humanChoice == "rock") {
+        if (computerChoice == "rock") {
+            console.log ("Tie! You both chose rock.");
+        } else if (computerChoice == "scissors") {
+            console.log ("You win! Rock beats scissors.");
+        } else if (computerChoice == "paper"){
+            console.log ("Computer wins! Paper beats rock.");
+        }
+    }
+    //Tests win conditions when user inputs paper
+    if (humanChoice == "paper") {
+        if (computerChoice == "rock") {
+            console.log ("You win! Paper beats rock.");
+        } else if (computerChoice == "scissors") {
+            console.log ("You lose! Scissors beats paper.");
+        } else if (computerChoice == "paper"){
+            console.log ("Tie! You both chose paper.");
+        }
+    }
+    //Tests win conditions when user inputs scissors
+    if (humanChoice == "scissors") {
+        if (computerChoice == "rock") {
+            console.log ("You lose! Rock beats scissors.");
+        } else if (computerChoice == "scissors") {
+            console.log ("Tie! You both chose scissors.");
+        } else if (computerChoice == "paper"){
+            console.log ("You win! Scissors beats paper.");
         }
     }
 }
@@ -30,3 +65,5 @@ console.log(computerSelection);
 
 humanSelection = getHumanChoice();
 console.log(humanSelection);
+
+playRound(humanSelection, computerSelection);
